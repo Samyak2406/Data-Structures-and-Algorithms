144084cd378f487e93b577e773153db481afc48f
HetDaftary <hetdaftary@gmail.com>
2020-10-02 16:07:03 +0530

    Adding split and join with vectors for c++

--- Basic functions of Python for C++/test.cpp
+++ Basic functions of Python for C++/test.cpp
@@ -1,4 +1,5 @@
 #include "Split_And_Join.hpp"
+#include "Split_And_Join_with_Vectors.hpp"
 
 int main() {
     string array[2] = {"Het", "Daftary"};
@@ -9,5 +10,14 @@ int main() {
     string* ans_for_test_string = split_String(test_string, " ", 2);
     cout << ans_for_test_string[0] << endl;
     cout << ans_for_test_string[1] << endl; 
+
+    vector<string> data = {"Het", "Daftary"};
+    cout << join_Strings_vector(data, " ") << endl;
+    cout << join_Strings_vector(data, "\n") << endl;
+
+    string str = "Het Daftary";
+    vector<string> ans = split_String_vector(str, " ");
+    cout << ans[0] << endl;
+    cout << ans[1] << endl;
     return 0;
 }
