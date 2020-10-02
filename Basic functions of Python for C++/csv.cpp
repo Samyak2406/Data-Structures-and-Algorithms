6c46cb6c0161a29d7c079a2a3a7c52addd273a74
HetDaftary <hetdaftary@gmail.com>
2020-10-02 16:34:20 +0530

    Adding support to read, write and generate random csv files CSV in C++

--- Basic functions of Python for C++/csv.cpp
+++ Basic functions of Python for C++/csv.cpp
@@ -8,8 +8,8 @@
 #define number_of_fields 5
 
 int main() {
-    csv_writer(csv_reader("csv_files/Sample100.csv", ",", number_of_fields), ", ", number_of_fields, "../Results/C++/1.csv");
-    csv_writer(csv_reader("csv_files/Sample500.csv", ",", number_of_fields), ", ", number_of_fields, "../Results/C++/2.csv");
+    csv_writer(csv_reader("csv_files/Sample100.csv", ",", number_of_fields), ", ", number_of_fields, "Results/without_vectors/1.csv");
+    csv_writer(csv_reader("csv_files/Sample500.csv", ",", number_of_fields), ", ", number_of_fields, "Results/without_vectors/2.csv");
     
     // [['int', 5, 10], ['float', 4, 6], ['alphabets_lower_Case', 5], ['alphabets_upper_Case', 7], ['random_string', 4]].
     vector<Field> fields = {Field("int", 5, 10), Field("float", 4.0, 6.0), Field("alphabets_lower_Case", 5), Field("alphabets_upper_Case", 7), Field("random_string", 4)};
