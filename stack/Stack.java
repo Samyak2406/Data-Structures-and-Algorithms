388377d01fa06f92897785258448240e8f052d07
Akshat2404 <60111945+Akshat2404@users.noreply.github.com>
2020-10-05 18:25:54 +0530

    Create Stack.java
    Simple Stack in Java

---
+++ stack/Stack.java
@@ -0,0 +1,72 @@
+import java.util.*;
+class Main
+{
+    static int top = -1;
+    static int size;
+    static void push(int[] a)
+    {
+        Scanner in = new Scanner(System.in);
+        if(top == size-1)
+        {
+            System.out.println("Stack Overflow");
+            return;
+        }
+        System.out.print("Enter the number to be added in the Stack:   ");
+        int num = in.nextInt();
+        top++;
+        a[top] = num;
+    }
+    static void pop(int[] a)
+    {
+        if(top == -1)
+        {
+            System.out.println("Stack UnderFlow");
+            return;
+        }
+        int del = a[top];
+        top--;
+        System.out.println("Popped element is : " + del);
+    }
+    static void display(int[] a)
+    {
+        int i;
+        if(top == -1)
+        {
+            System.out.println("Empty Stack");
+            return;
+        }
+        for(i=top;i>=0;i--)
+            System.out.println("|_____" + a[i] + "_____|");
+    }
+    public static void main (String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+	    System.out.println ("----------STACK----------");
+        System.out.print("Enter the size of the array :    ");
+        size = sc.nextInt();
+        int[] stack = new int[size];
+        int n;
+        do {
+	    System.out.println ("1. Push");
+	    System.out.println ("2. Pop");
+	    System.out.println ("3. Display");
+	    System.out.println("Any other number to exit");
+	    System.out.print ("Enter your choice :    ");
+        n = sc.nextInt();
+        switch(n)
+        {
+            case 1:
+                push(stack);
+                break;
+            case 2:
+                pop(stack);
+                break;
+            case 3:
+                display(stack);
+                break;
+            default:
+                break;
+        }
+        }while(n==1 || n==2 || n==3);
+    }
+}
